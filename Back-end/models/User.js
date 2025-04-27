d46f93e797338dda3aeb6299b785eab3c89f591b
Madhu Kunchala <madhu@smartsolutionsdigi.com>
2025-04-27 23:27:29 +0530

    changes

--- Back-end/models/User.js
+++ Back-end/models/User.js
@@ -1,14 +1,53 @@
+// models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
-    unique: true,
-    lowercase: true
+    unique: true
   },
-  password: { type: String, required: true },
-}, { timestamps: true });
+  password: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['online', 'offline', 'away'],
+    default: 'offline'
+  },
+  statusUpdatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = mongoose.model('User', userSchema);
+// Password hashing pre-save hook
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Status update hook
+UserSchema.pre('save', function(next) {
+  if (this.isModified('status')) {
+    this.statusUpdatedAt = Date.now();
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
